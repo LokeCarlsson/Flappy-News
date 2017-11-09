db6f1f5d90af8ead4e9492261b10a1be494ae045
Loke Carlsson <lc222ak@student.lnu.se>
2017-11-09 21:33:24 +0100

    Added y movement

--- app/displayobjects/Hero/Hero.js
+++ app/displayobjects/Hero/Hero.js
@@ -19,12 +19,16 @@ export default class Hero extends Sprite {
     this.interactive = true
 
     this.collided = false
-    this.speedX = 5 
+    this.flap = false
+    this.speedX = 5
+    this.speedY = 5
     
-    window.addEventListener('mousemove', this.collision.bind(this))
+    window.addEventListener('click', this.collision.bind(this))
+    window.addEventListener('mousemove', this.flappy.bind(this))
 
     Store.subscribe(() => {
       this.move()
+      this.gravity()
     })
   }
 
@@ -32,21 +36,47 @@ export default class Hero extends Sprite {
     this.collided = true
   }
 
+  flappy() {
+    this.flap = true
+  }
+  
   decreaseLife() {
     this.life--
   }
 
+  gravity() {
+    if (this.flap) {
+      this.decreaseSpeedY()
+      this.fall()
+    } else {
+      this.increaseSpeedY()
+    }
+    this.position.y += this.speedY
+  }
+
+  fall() {
+    if (this.speedY <= -5) {
+      this.flap = false
+    }
+  }
+
   move() {
     if (this.collided) {
-      this.decreaseSpeed()
+      this.decreaseSpeedX()
       this.removeCollided()
     } else {
-      this.increaseSpeed()
+      this.increaseSpeedX()
     }
     this.position.x += this.speedX
   }
 
-  decreaseSpeed() {
+  removeCollided() {
+    if (this.speedX <= -5) {
+      this.collided = false
+    }
+  }
+
+  decreaseSpeedX() {
     if (this.speedX > 4) {
       this.speedX -= 20
     } else if (this.speedX > 2) {
@@ -56,13 +86,7 @@ export default class Hero extends Sprite {
     }
   }
 
-  removeCollided() {
-    if (this.speedX <= -5) {
-      this.collided = false
-    }
-  }
-
-  increaseSpeed() {
+  increaseSpeedX() {
     if (this.speedX < 2) {
       this.speedX += 0.5
     } else if (this.speedX < 4) {
@@ -71,4 +95,39 @@ export default class Hero extends Sprite {
       this.speedX += 0.2
     }
   }
+
+  decreaseSpeedY() {
+    if (this.speedY > 4) {
+      this.speedY -= 20
+    } else if (this.speedY > 2) {
+      this.speedY -= 1
+    } else {
+      this.speedY -= 0.1
+    }
+  }
+
+  increaseSpeedY() {
+    if (this.speedY < 2) {
+      this.speedY += 0.5
+    } else if (this.speedY < 4) {
+      this.speedY += 1
+    } else if (this.speedY <= 5) {
+      this.speedY += 0.2
+    }
+  }
+
+  // drop() {
+  //   Store.subscribe(() => {
+  //     const { tick, previousTick } = Store.getState().Animation
+  //     if (tick !== previousTick) {
+  //       this.position.y -= 0.1
+  //     }
+  //     if (this.position.y < -12) {
+  //       this.position.y = -12
+  //     }
+  //     if (this.position.y > Store.getState) {
+  //       this.position.y= 12
+  //     }
+  //   })
+  // }
 }
