2a27edb534e43c62e3f2ec6f7c92aafd7f931e27
Loke Carlsson <lc222ak@student.lnu.se>
2017-11-09 10:56:44 +0100

    Added project ID to config example

--- config_example.js
+++ config_example.js
@@ -2,6 +2,7 @@ export default {
   apiKey: "<API_KEY>",
   authDomain: "<PROJECT_ID>.firebaseapp.com",
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  projectId: "flappy-news",  
   storageBucket: "<BUCKET>.appspot.com",
   messagingSenderId: "xxxxxxxxxxxx"
 }
