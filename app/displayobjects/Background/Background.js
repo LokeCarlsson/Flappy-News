27c0e6aa7ed5899ad4a35f9d4ac2d133417adc93
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-11-10 13:57:27 +0100

    tiling bg

--- app/displayobjects/Background/Background.js
+++ app/displayobjects/Background/Background.js
@@ -1,6 +1,7 @@
-import { Container, Sprite, BLEND_MODES } from 'pixi.js';
+import { Container, extras, Sprite, Texture, BLEND_MODES } from 'pixi.js';
 import BG from './BG.png';
 import FG from './FG.png';
+import Foreground from './Foreground/Foreground'
 import Store from '../../stores/Store'
 
 /**
@@ -14,18 +15,18 @@ export default class Background extends Container {
   constructor() {
     super();
 
-    this.bg = Sprite.fromImage(BG);
-    this.fg = Sprite.fromImage(FG);
-
+    this.bg = Sprite.fromImage(BG)
+    this.fg = Texture.fromImage(FG)
+    this.fore = new extras.TilingSprite(this.fg, 1930, 1080)
     this.animate()
-
+    
     this.addChild(this.bg);
-    this.addChild(this.fg);
+    this.addChild(this.fore);
   }
 
   animate() {
     Store.subscribe(() => {
-      this.fg.position.x -= 0.5;
+      this.fore.tilePosition.x -= 0.5;
     })
   }
 }
---
+++ app/displayobjects/Background/Foreground/Foreground.js
@@ -0,0 +1,17 @@
+import { Sprite, Texture, extras } from 'pixi.js'
+import FG from '../FG.png'
+
+export default class Foreground extends Sprite {
+  constructor() {
+    const texture = Texture.fromImage(FG)
+    super(texture)
+    // const tiled = new extras.TilingSprite(texture, 1000, 1000)
+    // animate()
+  }
+
+  // animate() {
+  //   Store.subscribe(() => {
+  //     this.fg.position.x -= 0.5;
+  //   })
+  // }
+}
